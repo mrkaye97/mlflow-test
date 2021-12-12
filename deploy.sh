1f69f2f35084a224cf3a331a6d1713bd367413b6
mrkaye97 <mrkaye97@gmail.com>
2021-12-12 17:00:18 -0500

    set heroku config from sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 heroku login
 heroku container:login
 
+heroku config:set $(confcrypt aws read env.econf --format="%n=%v" | tr '\n' ' ') --app mk-mlflow-test >> /dev/null
+
 heroku container:push web --app mk-mlflow-test
 heroku container:release web --app mk-mlflow-test
